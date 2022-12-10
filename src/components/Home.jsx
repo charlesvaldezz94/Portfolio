b11e3f60bab07786dfb45a1c171c5b807377253a
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-12-10 14:19:11 -0600

    home page template

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,30 @@
 import React from "react"
 
 const Home = () => {
+
     return (
         <div className="homeContainer">
-            <h2>I am home </h2> 
+            <div id="profilePic">
+                profile pic here
+            </div>
+            <div id="homeBody">
+                <div className="homeSquares">
+                    <h2> Hello! I'm Charles </h2>
+                    <p> I'm an entry level fullstack software engineer with knowledge in html, CSS, and javascript. Checkout my recent projects and resume here on this site!</p> 
+                </div>
+                <div className="homeSquares">
+                    image 1
+                </div>
+                <div className="homeSquares">
+                    image 1
+                </div>
+                <div className="homeSquares">
+                    image 1
+                </div>
+                <div className="homeSquares">
+                    image 1
+                </div>
+            </div>
         </div>
 
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./Navbar"
+import {Navbar, Home} from "./"
 
 const Main = () => {
   return <div id="main">
     <Navbar />
-    <h1> I am main </h1>
+    <Home />
   </div>;
 };
 
